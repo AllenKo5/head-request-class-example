61c04a6492b8ec1c391268dd17f40219abd3ad5f
Allen Ko (RIT Student) <ask9458@rit.edu>
2022-09-14 15:48:58 -0400

    finished head request example

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -13,32 +13,46 @@ const respondJSON = (request, response, status, object) => {
 };
 
 const respondJSONMeta = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
 
+  response.writeHead(status, headers);
+  response.end();
 };
 
 const getUsers = (request, response) => {
+  const responseJSON = {
+    users,
+  };
 
+  return respondJSON(request, response, 200, responseJSON);
 };
 
 const getUsersMeta = (request, response) => {
-
+  return respondJSONMeta(request, response, 200);
 };
 
 const updateUser = (request, response) => {
+  const newUser = {
+    createdAt: Date.now(),
+  };
+  users[newUser.createdAt] = newUser;
 
+  return respondJSON(request, response, 201, newUser);
 };
 
 const notFound = (request, response) => {
   const responseJSON = {
     message: 'The page you are looking for was not found.',
     id: 'notFound',
-  }
+  };
 
   return respondJSON(request, response, 404, responseJSON);
 };
 
 const notFoundMeta = (request, response) => {
-
+  return respondJSONMeta(request, response, 404);
 };
 
 module.exports = {
